742d740e463548b52450a3260aed6bab5940a853
kuki236 <sebasandree729@gmail.com>
2024-06-01 20:17:05 -0500

    paginaPrincipal

---
+++ js/perros.js
@@ -0,0 +1,115 @@
+const perro2={
+  nombre:"Thomas",
+  sexo:"Macho",
+  tamaño:"Mediano",
+  edad:"5 años"
+}
+const perro26={
+  nombre:"Clint",
+  sexo:"Macho",
+  tamaño:"Pequeño",
+  edad:"4 años"
+}
+const perro28={
+  nombre:"Leo",
+  sexo:"Macho",
+  tamaño:"Mediano",
+  edad:"5 años"
+}
+
+const perro13={
+  nombre:"Bono",
+  sexo:"Macho",
+  tamaño:"Grande",
+  edad:"4.5 años"
+}
+const perro15={
+  nombre:"Max",
+  sexo:"Macho",
+  tamaño:"Mediano",
+  edad:"6 años"
+}
+const perro19={
+  nombre:"Alfie",
+  sexo:"Macho",
+  tamaño:"Pequeño",
+  edad:"4 años"
+}
+const perro20={
+  nombre:"Gringo",
+  sexo:"Macho",
+  tamaño:"Grande",
+  edad:"4.5 años"
+}
+const perro30={
+  nombre:"Tigre",
+  sexo:"Macho",
+  tamaño:"Mediano",
+  edad:"3 años"
+}
+const perro36={
+  nombre:"Romi",
+  sexo:"Hembra",
+  tamaño:"Mediana",
+  edad:"4 años"
+}
+const perro65={
+  nombre:"Lauro",
+  sexo:"Macho",
+  tamaño:"Mediano",
+  edad:"3 años"
+}
+const perro67={
+  nombre:"Toño",
+  sexo:"Macho",
+  tamaño:"Mediano",
+  edad:"8 meses"
+}
+const perro34={
+  nombre:"Carmelita",
+  sexo:"Hembra",
+  tamaño:"Mediana",
+  edad:"4 años"
+}
+const perro25={
+  nombre:"Tomasita",
+  sexo:"Hembra",
+  tamaño:"Mediana",
+  edad:"3 años"
+}
+const perro22={
+  nombre:"Brunito",
+  sexo:"Macho",
+  tamaño:"Mediano",
+  edad:"4 años"
+}
+const perro23={
+  nombre:"Bambi",
+  sexo:"Hembra",
+  tamaño:"Mediana",
+  edad:"3 años"
+}
+const perro27={
+  nombre:"Charlie",
+  sexo:"Macho",
+  tamaño:"Mediano",
+  edad:"5 años"
+}
+const perro35={
+  nombre:"Manu",
+  sexo:"Macho",
+  tamaño:"Pequeño",
+  edad:"3 años"
+}
+const perro11={
+  nombre:"Soco",
+  sexo:"Macho",
+  tamaño:"Mediano",
+  edad:"7 años"
+}
+const rango1=[perro2];
+const rango2=[perro26,perro28];
+const rango3=[perro13, perro15, perro19, perro20, perro30, perro36, perro65, perro67];
+const rango4=[perro25,perro34];
+const rango5=[perro22, perro23, perro27,perro35];
+const rango6=[perro11];
---
+++ main.js
@@ -0,0 +1,4 @@
+const enlaceDogMatch = document.querySelector('.botonIniciar');
+enlaceDogMatch.addEventListener('click',function(){
+    window.open('https://kuki236.github.io/DogMatch/', '_blank');
+})
