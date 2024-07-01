7927a1dadaccade6f643ebca291aa543e0e17715
kuki236 <sebasandree729@gmail.com>
2024-06-30 20:10:59 -0500

    agregar detalles finales
    se agrega logica para automatizar los perros de la entrada

--- main.js
+++
@@ -1,4 +0,0 @@
-const enlaceDogMatch = document.querySelector('.botonIniciar');
-enlaceDogMatch.addEventListener('click',function(){
-    window.open('https://kuki236.github.io/DogMatch/', '_blank');
-})
